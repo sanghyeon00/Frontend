68eedba2dd34036bf379ccb804ce43490d936fc8
snaghyeon00 <rlftkdgus123@gmail.com>
2024-04-10 19:08:28 +0900

    testlog

--- Fanatic-React-Template/src/App.js
+++ Fanatic-React-Template/src/App.js
@@ -5,16 +5,11 @@ import { Helmet } from "react-helmet";
 import Landing from "./screens/Landing.jsx";
 import CreateQpage from "./components/Sections/CreateQpage.jsx";
 import Classroom from "./components/Sections/Classroom.jsx";
-<<<<<<< HEAD
 import TopNavbar from "./components/Nav/TopNavbar.jsx";
 
 //Member
 import Login from "./components/Member/Login.jsx";
 import Membership from "./components/Member/Membership.jsx"
-=======
-// Top Navbar Component
-import TopNavbar from "./components/Nav/TopNavbar"; // TopNavbar 컴포넌트 경로에 따라 수정해주세요.
->>>>>>> 8b7fa72125f8586e9efb00beb3e0736b03c3c306
 
 export default function App() {
   return (
@@ -25,11 +20,7 @@ export default function App() {
           <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
           <link href="https://fonts.googleapis.com/css2?family=Khula:wght@400;600;800&display=swap" rel="stylesheet" />
         </Helmet>
-<<<<<<< HEAD
         <TopNavbar />
-=======
-        <TopNavbar /> {/* 모든 페이지 상단에 TopNavbar 컴포넌트를 표시 */}
->>>>>>> 8b7fa72125f8586e9efb00beb3e0736b03c3c306
         <Routes>
           <Route path="/" element={<Landing />} />
           <Route path="/create_question" element={<CreateQpage />}/>
